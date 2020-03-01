b126f7d8fd8664a52d499062d46f10587726e627
Felipe Neves <felipe.alneves@outlook.com>
2020-03-01 16:32:42 -0300

    Commit inicial contendo conceito Builder

---
+++ README.md
@@ -0,0 +1,14 @@
+Repositório destinado a explicação e exemplificação
+de vários conceitos e técnicas da linguagem Java.
+
+Se trata de um projeto Maven simples, contendo apenas
+o Lombok como dependencia para evitar codigo
+verboso em excesso.
+
+Cada pacote será nomeado de acordo com o conceito 
+e/ou técnica que irá abordar, juntamente com uma classe 
+Main onde será mostrado a implementação 
+do mesmo.
+
+Basta clonar o repositório e executar a classe main 
+desejada.
---
+++ src/main/java/br/com/concepts/builder/Main.java
@@ -0,0 +1,24 @@
+package br.com.concepts.builder;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String... args) {
+
+        // Pela ausência de construtores publicos, se obriga a utilização do builder.
+        // A classe será imutável. Sendo impossível chamar Pessoa pessoa = new Pessoa();
+
+        // Os parâmetros obrigatorios são passados no construtor do builder.
+        // Os demais são opcionais. Nesse exemplo, só setei dataNascimento e profissão.
+        Pessoa pessoa = new Pessoa.Builder("Felipe", "Neves", "00000000")
+                .dataNascimento(LocalDate.of(1993, 4, 21))
+                .profissao("Programador")
+                .build();
+
+        //Felipe
+        System.out.println(pessoa.getNome());
+
+        // Null
+        System.out.println(pessoa.getEmail());
+    }
+}
