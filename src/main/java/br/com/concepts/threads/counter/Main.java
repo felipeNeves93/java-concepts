72813a85cc25a1efe2ec0e65fea45b6b6ecf492e
Felipe Neves <felipe.alneves@outlook.com>
2021-05-10 08:55:20 -0300

    Added explanation of thread variables and scope

--- src/main/java/br/com/concepts/threads/counter/Main.java
+++ src/main/java/br/com/concepts/threads/counter/Main.java
@@ -14,6 +14,11 @@ public class Main {
         var countdownThread2 = new CountdownThread(countdown);
         countdownThread2.setName("Thread 2");
 
+        // One important concept is about the scope of the variables within the threads execution.
+        // If a variable is declared inside the scope of a method for example, it will use the "Thread stack",
+        // meaning that the variable wont be shared between threads.
+        // However, if we declare an instance variable, it will be stored inside the Heap memory, being
+        // Shared amongst all the executions.
         countdownThread1.start();
         countdownThread2.start();
     }
